450eb968711094c08f5d0112dea4de76bacbd555
Georgi Strelkov <georg.strelkov@mail.ru>
2023-09-27 01:29:22 +0300

    feat: add guessing-game task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.res = this.max - this.min;
+        this.res = Math.floor(this.res/2);
+        this.res = this.max - this.res;
+        this.mid = this.res;
+        return this.mid;
     }
 
     lower() {
-
+        this.max = this.mid;
     }
 
     greater() {
-
+        this.min = this.mid;
     }
 }
 
